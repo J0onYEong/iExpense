4d3570478ddf836ec826b80b5ebc7d423d6ae5d4
choijun0 <cjy00050005@gmail.com>
2022-11-16 16:01:38 +0900

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by 최준영 on 2022/11/16.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
